efb396ce2aa7b0ac126fa3a44784d1cf442b64cb
vejonnadula <vejonandula@paypal.com>
2022-03-23 19:24:49 +0530

    changing the model

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("adding all ml related code here")
+
+def model1():
+    print("adding all related code for model1")
+
+def model2():
+    print("adding all related code for model2")
+
+def model3():
+    print("adding all related code for model3")
